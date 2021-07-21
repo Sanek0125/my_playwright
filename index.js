04b61b2212d0b267a9877442ff4750ae59f89361
Sanek0125 <a.p.primakov@gmail.com>
2021-07-21 11:56:30 +0300

    Первый коммит

---
+++ index.js
@@ -0,0 +1,34 @@
+const { chromium } = require('playwright');
+
+function getNumberFromString(string) {
+    return Number(string.replace(/[^\d]/gi, ''));
+}
+
+(async () => {
+  const browser = await chromium.launch();
+
+  const page = await browser.newPage();
+
+  await page.goto('https://twitter.com/webstandards_ru');
+
+  await page.waitForSelector(`text=/^Followers$/i`);
+
+  const span = await page.$(`text=/^Followers$/i`);
+  await page.screenshot({path: 'screenshot3.png'});
+
+    
+  const followers = await page.evaluate(async span => {
+    return span.parentNode.parentNode.getAttribute('title');
+  }, span);
+
+//   console.log(result); // prints "56"
+  
+
+  await browser.close();
+
+//   console.log(span);
+  console.log(getNumberFromString(followers));
+  
+})();
+
+// console.log(span);
